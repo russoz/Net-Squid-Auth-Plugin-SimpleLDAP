46a3e54661884503cc490c69e1475d9fc380277c
Alexei Znamensky <russoz@cpan.org>
2010-03-06 16:38:42 -0300

    DIST VERSION 0.1.13

--- Net-Squid-Auth-Plugin-SimpleLDAP/lib/Net/Squid/Auth/Plugin/SimpleLDAP.pm
+++ Net-Squid-Auth-Plugin-SimpleLDAP/lib/Net/Squid/Auth/Plugin/SimpleLDAP.pm
@@ -11,11 +11,11 @@ Net::Squid::Auth::Plugin::SimpleLDAP - A simple LDAP-based credentials validatio
 
 =head1 VERSION
 
-Version 0.1.12
+Version 0.1.13
 
 =cut
 
-use version; our $VERSION = qv('0.1.12');
+use version; our $VERSION = qv('0.1.13');
 
 =head1 SYNOPSIS
 
